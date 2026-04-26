f9c75f031fbce58f4e67c2be2e71f49005a6591a
sithija-ws <sithija.ws@gmail.com>
2026-04-26 22:05:59 +0530

    feat: add reset functionality and improve input placeholders in Calculator component

--- src/Calculator/page.jsx
+++ src/Calculator/page.jsx
@@ -8,15 +8,21 @@ function calculateBmi(height, weight){
 }
 
 
+
 const Calculator = () => {
   const [weight, setWeight] = useState("");
   const [height, setHeight] = useState("");
   const [bmi, setBmi] = useState(0);
 
+  const handleReset = ()=>{
+    setBmi(0);
+    setHeight("");
+    setWeight("");
+  }
   return (
     <div className='min-h-screen bg-[#090C1D] flex justify-center items-center'>
       <div 
-        className='bg-white/5 max-w-2xl w-full h-[350px] border-2 border-white/10
+        className='bg-white/5 max-w-2xl w-full h-87.5 border-2 border-white/10
         backdrop-blur-2xl rounded-xl  gap-4 mt-4 flex flex-col  items-center'>
         
         {/*head text*/}
@@ -31,12 +37,12 @@ const Calculator = () => {
 
         {/*inputs */}
         <div className='flex flex-col gap-3 pt-2 items-start justify-start w-70'>
-            <input type='number' placeholder='Weight' onChange={(e)=>{setWeight(e.target.value)}}
+            <input type='number' value={weight} placeholder='Weight in kg' onChange={(e)=>{setWeight(e.target.value)}}
                 className='w-full bg-white/10 border border-white/10 text-white rounded-md placeholder:text-[#64748B]
                 focus:outline-none focus:ring-2 focus:ring-fuchsia-500/40 text-center px-4 py-2'
             />
 
-            <input type='number' placeholder='Height' onChange={(e)=>{setHeight(e.target.value)}}
+            <input type='number' value={height} placeholder='Height in cm' onChange={(e)=>{setHeight(e.target.value)}}
                 className='w-full bg-white/10 border border-white/10 text-white rounded-md placeholder:text-[#64748B]
                 focus:outline-none focus:ring-2 focus:ring-fuchsia-500/40 text-center px-4 py-2'
             />
@@ -52,7 +58,7 @@ const Calculator = () => {
                 Calculate
             </button>
 
-            <button
+            <button onClick={()=>{handleReset()}}
               className='w-22 h-9 bg-white/5 hover:bg-red-500/10 border border-red-500/50 rounded-md text-red-400 font-semibold tracking-wide text-[17px]'>
                 Reset
             </button>
